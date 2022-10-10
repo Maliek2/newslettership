e5625e46ad0da991832938f1c172abe30a26bf6b
Maliek2 <104944194+Maliek2@users.noreply.github.com>
2022-10-10 23:24:12 +0200

    Create JitpackDemo.java

---
+++ app/src/main/java/app/vercel/newslettership/JitpackDemo.java
@@ -0,0 +1,10 @@
+package app.vercel.newslettership;
+
+import android.util.Log;
+
+public class JitpackDemo {
+
+    public static void logEnvironment() {
+        Log.d(JitpackDemo.class.toString(), BuildConfig.ENV);
+    }
+}
